77d44bf249c692ac5444b3c6101f6cc75dc17302
Tim Rooke <timrooke1991@gmail.com>
2021-06-23 09:05:19 +0100

    Basic button setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PrimaryButton from './components/Buttons';
 
-const App = () => <h1>Hello world!</h1>
+const App = () => (
+  <PrimaryButton>Primary button</PrimaryButton>
+);
 
 ReactDOM.render(<App />, document.querySelector('#root'));

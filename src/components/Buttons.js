2b78572725d749dc528eafc3416c40616af5a252
Tim Rooke <timrooke1991@gmail.com>
2021-06-30 08:53:05 +0100

    Use themes, colors and typography utils

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,32 +1,28 @@
 import styled from "styled-components";
-
-const primary = '#596E6D';
-const secondary = '#3DBFB9';
-const tertiary = '#C4C4C4';
+import { defaultTheme } from "../utils";
+import { typeScale } from "../utils";
 
 const Button = styled.button`
-  padding: 12px 24px;
-  font-size: 1rem;
+  padding: 8px 12px;
+  font-size: ${typeScale.paragraph};
   border-radius: 2px;
   min-width: 100px;
   cursor: pointer;
-  font-family: "Roboto Mono", monospace;
-`
+  font-family: ${defaultTheme.primaryFont};
+`;
 
 export const PrimaryButton = styled(Button)`
-  background-color: ${primary};
-  border: none;
-  color: white;
-`
+  background-color: ${defaultTheme.primaryColor};
+  color: ${defaultTheme.textColorOnPrimary};
+  border: 2px solid transparent;
+`;
 
 export const SecondaryButton = styled(Button)`
-  background-color: ${secondary};
-  border: none;
-  color: #333;
-`
+  border: 2px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
+`;
 
 export const TertiaryButton = styled(Button)`
-  background-color: ${tertiary};
-  border: none;
-  color: #333;
-`
+  border: 2px solid transparent;
+  color: ${defaultTheme.primaryColor};
+`;

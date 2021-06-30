e54b238421e97728dce5ea87b27429fe9b990e16
Tim Rooke <timrooke1991@gmail.com>
2021-06-30 09:01:21 +0100

    Add polished; Create global styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { PrimaryButton, SecondaryButton, TertiaryButton } from './components/Buttons';
+import { GlobalStyle } from './utils';
 
 const App = () => (
   <React.Fragment>
     <PrimaryButton>Primary button</PrimaryButton>
     <SecondaryButton>Secondary Button</SecondaryButton>
     <TertiaryButton>Tertiary Button</TertiaryButton>
+    <GlobalStyle />
   </React.Fragment>
 );
 
